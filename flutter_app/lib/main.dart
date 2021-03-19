b52f3e49838dfe93b4b5749bb1e5f7278a28641c
unknown <49125967+SaoodZafar@users.noreply.github.com>
2021-03-19 17:09:08 +0500

    first

--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -9,20 +9,78 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        home: Scaffold(
-          appBar: AppBar(
-            backgroundColor: Colors.black,
-            title: Text("Profile",
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.black,
+          leading: Icon(Icons.menu),
+          title: Center(
+            child: Text(
+              "Profile",
               style: TextStyle(
                 color: Colors.white,
               ),
             ),
-
           ),
-        )
+          actions: [Icon(Icons.settings)],
+        ),
+        body: Column(
+          children: [
+            Center(
+              child: Stack(
+                children: [
+                  Container(
+                    alignment: Alignment.center,
+                    child: Image(
+                      image: AssetImage("../images/ij.jpg"),
+                      height: 400.0,
+                      width: double.infinity,
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                  Container(
+                    alignment: Alignment.center,
+                    child: Text(
+                      "Looking Owsem",
+                      style: TextStyle(
+                        color: Colors.deepPurple,
+                        fontSize: 22.0,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
+            Container(
+              height: 70.0,
+              color: Colors.blueAccent,
+                child:Row(
 
+                  children: [
 
+                    Text("Event Manager "
+                        "Event Manager ",
+                    style: TextStyle(
+                      fontSize: 12.0,
+                      color: Colors.white,
+
+                    ),
+                      maxLines: 2,
+                    ),
+                    Text("Event Manager",
+                      style: TextStyle(
+                        fontSize: 12.0,
+                        color: Colors.white,
+
+                      ),
+                      maxLines: 2,
+                    )
+                  ],
+                ))
+          ],
+        ),
+      ),
     );
   }
-
 }

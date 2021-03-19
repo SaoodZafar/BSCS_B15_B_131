6f21cfe3d5351c7239a44faa4b7a396be32e906d
unknown <49125967+SaoodZafar@users.noreply.github.com>
2021-03-19 18:18:08 +0500

    Update main.dart

--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -33,7 +33,7 @@ class MyApp extends StatelessWidget {
                     alignment: Alignment.center,
                     child: Image(
                       image: AssetImage("../images/ij.jpg"),
-                      height: 400.0,
+                      height: 350.0,
                       width: double.infinity,
                       fit: BoxFit.cover,
                     ),
@@ -53,32 +53,117 @@ class MyApp extends StatelessWidget {
               ),
             ),
             Container(
-              height: 70.0,
-              color: Colors.blueAccent,
-                child:Row(
-
+                padding: EdgeInsets.all(20.0),
+                color: Colors.blueAccent,
+                child: Row(
                   children: [
-
-                    Text("Event Manager "
-                        "Event Manager ",
-                    style: TextStyle(
-                      fontSize: 12.0,
-                      color: Colors.white,
-
+                    Column(
+                      children: [
+                        Text(
+                          "Event Manager",
+                          style: TextStyle(
+                            fontSize: 12.0,
+                            color: Colors.white,
+                          ),
+                        ),
+                        Text(
+                          "San Fransico",
+                          style: TextStyle(
+                            fontSize: 12.0,
+                            color: Colors.white,
+                          ),
+                        )
+                      ],
                     ),
-                      maxLines: 2,
-                    ),
-                    Text("Event Manager",
-                      style: TextStyle(
-                        fontSize: 12.0,
-                        color: Colors.white,
-
-                      ),
-                      maxLines: 2,
-                    )
                   ],
-                ))
+                )),
+            Container(
+              padding: EdgeInsets.all(20.0),
+              color: Colors.blueAccent,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Column(
+                    children: [
+                      Text(
+                        "145K",
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 22.0,
+                          letterSpacing: 2.0,
+                          color: Colors.white,
+                        ),
+                      ),
+                      Text(
+                        "Follwors",
+                        style: TextStyle(
+                          fontSize: 12.0,
+                          letterSpacing: 3.0,
+                          color: Colors.white,
+                        ),
+                      )
+                    ],
+                  ),
+                  Column(
+                    children: [
+                      Text(
+                        "56K",
+                        style: TextStyle(
+                          fontSize: 22.0,
+                          color: Colors.white,
+                          letterSpacing: 2.0,
+                        ),
+                      ),
+                      Text(
+                        "Following",
+                        style: TextStyle(
+                          fontSize: 12.0,
+                          letterSpacing: 3.0,
+                          color: Colors.white,
+                        ),
+                      )
+                    ],
+                  ),
+                  Column(
+                    children: [
+                      Text(
+                        "1,690",
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          letterSpacing: 2.0,
+                          fontSize: 22.0,
+                          color: Colors.white,
+                        ),
+                      ),
+                      Text(
+                        "Likes",
+                        style: TextStyle(
+                          fontSize: 12.0,
+                          letterSpacing: 3.0,
+                          color: Colors.white,
+                        ),
+                      )
+                    ],
+                  ),
+                ],
+              ),
+            )
           ],
+        ),
+
+        bottomNavigationBar: BottomAppBar(
+          color: Colors.white10,
+          notchMargin: 2.0,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+
+            children: [
+
+              Icon(Icons.sms),
+              Icon(Icons.call),
+              Icon(Icons.camera_alt),
+            ],
+          ),
         ),
       ),
     );

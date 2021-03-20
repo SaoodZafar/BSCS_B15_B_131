faa1e041cd3cb6cbe224ba9468ede3e9d0bc48ef
unknown <49125967+SaoodZafar@users.noreply.github.com>
2021-03-20 20:59:32 +0500

    last

---
+++ flutter_app3rd/lib/scr1.dart
@@ -0,0 +1,111 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_app3rd/clip.dart';
+import 'package:flutter_app3rd/colors1.dart';
+import 'package:flutter_app3rd/logIn.dart';
+import 'package:flutter_app3rd/logos.dart';
+
+class LoginScreen extends StatefulWidget {
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+
+        height: MediaQuery.of(context).size.height,
+        decoration: BoxDecoration(color: Colors.grey[200]),
+        child: Stack(children: <Widget>[
+          ClipPath(
+
+            clipper: MyCustomClipper(),
+            child: Container(
+
+              height: MediaQuery.of(context).size.height * 0.60,
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  colors: [
+                    AppColors.secondaryColor,
+                    Theme.of(context).primaryColor,
+                  ],
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter,
+                ),
+              ),
+
+            ),
+          ),
+
+          SafeArea(
+
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                CompanyLogo(),
+                Container(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: const <Widget>[
+                      Padding(
+                        padding: EdgeInsets.fromLTRB(50,0,0,0),
+                        child: Icon(
+                          Icons.chat_sharp,
+                          color: Colors.yellow,
+                          size: 100.0,
+
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                SizedBox(height: 30.0),
+                Login(),
+                SizedBox(height: 30.0),
+
+                Center(
+
+
+                  child: Row(
+
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        'Not Registered? ',
+                        style: TextStyle(
+                          color: Colors.grey[800],
+                          fontSize: 18.0,
+                        ),
+                      ),
+                      Text('Create Account',
+                        style: TextStyle(
+                          color: Colors.grey[800],
+                          fontWeight: FontWeight.bold,
+                          fontSize: 18.0,
+                        ),
+                      ),
+
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+
+        ],
+        ),
+
+      ),
+
+    );
+
+  }
+
+}
+
+mixin widget {
+
+}
---
+++ flutter_applasttry/android/app/src/main/kotlin/com/example/flutter_applasttry/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_applasttry
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}

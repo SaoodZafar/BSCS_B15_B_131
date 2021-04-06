0e67b7522204a81359608052a9a9662583ab0c4d
unknown <49125967+SaoodZafar@users.noreply.github.com>
2021-04-06 15:35:56 +0500

    Update main.dart

--- diceapp/lib/main.dart
+++ diceapp/lib/main.dart
@@ -1,7 +1,9 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'dart:math';
 
 import 'package:flutter/widgets.dart';
+
 void main() {
   return runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
@@ -15,108 +17,158 @@ void main() {
     ),
   ));
 }
+
 class Dice extends StatefulWidget {
   @override
   _DiceState createState() => _DiceState();
 }
 
-class _DiceState extends State<Dice>{
+class _DiceState extends State<Dice> {
   var dice1_image = 1;
   var dice2_image = 1;
   var dice3_image = 1;
   var dice4_image = 1;
   var counter1 = 0;
   var counter2 = 0;
-  var n= 0;
-  var x= 0;
-  void _incerment(){
+  var counter3 = 0;
+  var counter4 = 0;
+  var n = 0;
+  var x = 0;
+  var y=0;
+  var z=0;
+  int a=0;
+  int b=0;
+  int s=0;
+  void findmax(n,x,y,z){
+    a=max(n, x);
+    b=max(y, z);
+    s=max(a, b);
 
-  setState(() {
-    counter1++;
-    counter2++;
-    n = n + dice1_image;
-    x = x + dice2_image;
-  });
 
 
   }
-@override
+  void _incerment1() {
+    setState(() {
+      counter1++;
+
+      n = n + dice1_image;
+    });
+  }
+  void _incerment2() {
+    setState(() {
+      counter2++;
+
+      x = x + dice2_image;
+    });
+  }
+  void _incerment3() {
+    setState(() {
+      counter3++;
+
+      y = y + dice3_image;
+    });
+  }
+  void _incerment4() {
+    setState(() {
+      counter4++;
+
+      z = z + dice4_image;
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-  return Column(
-    children: [
-      Row(
-        children: [
-          Expanded(
-            child: FlatButton(onPressed:(){
-              setState(() {
-                dice1_image = Random().nextInt(5)+1;
-                _incerment();
-                print("Value1 = $counter1");
-                print("Value1 = $n");
-              });
-
-
-            },
-                child:
-            Image(image: new AssetImage("../images/d$dice1_image.png"),)
+    return Column(
+      children: [
+        Row(
+          children: [
+            Expanded(
+              child: FlatButton(
+                  onPressed: () {
+                    setState(() {
+                      dice1_image = Random().nextInt(5) + 1;
+                      _incerment1();
+                      print("Totel Click 1 = $counter1");
+                      print("Sum 1 = $n");
+                    });
+                  },
+                  child: Image(
+                    image: new AssetImage("../images/d$dice1_image.png"),
+                  )),
             ),
+            Expanded(
+              child: FlatButton(
+                  onPressed: () {
+                    setState(() {
+                      dice2_image = Random().nextInt(5) + 1;
+                      _incerment2();
+                      print("Totel Click 2 = $counter2");
+                      print("Sum 2 = $x");
+                    });
+                  },
+                  child: Image(
+                    image: new AssetImage("../images/d$dice2_image.png"),
+                  )),
+            ),
+          ],
+        ),
+        SizedBox(
+          height: 20,
+          width: 20,
+        ),
+        Row(
+          children: [
+            Expanded(
+              child: FlatButton(
+                  onPressed: () {
+                    setState(() {
+                      dice3_image = Random().nextInt(5) + 1;
+                      _incerment3();
+                      print("Totel Click 3 = $counter3");
+                      print("Sum 3 = $y");
+                    });
+                  },
+                  child: Image(
+                    image: new AssetImage("../images/d$dice3_image.png"),
+                  )),
+            ),
+            Expanded(
+              child: FlatButton(
+                  onPressed: () {
+                    setState(() {
+                      dice4_image = Random().nextInt(5) + 1;
+                      _incerment4();
+                      print("Totel Click 4 = $counter4");
+                      print("Sum 4 = $z");
+                    });
+                  },
+                  child: Image(
+                    image: new AssetImage("../images/d$dice4_image.png"),
+                  )),
+            ),
+          ],
+        ),
+        Row(children: [
+          Text("Show Maximum",
+
+
           ),
           Expanded(
-            child: FlatButton(onPressed:(){
-              setState(() {
-                dice2_image = Random().nextInt(5)+1;
-                _incerment();
-                print("Value2 = $counter2");
-                print("Value2 = $x");
-              });
-
-
-            },
-                child:
-                Image(image: new AssetImage("../images/d$dice2_image.png"),)
-            ),
+            child: FlatButton(
+                onPressed: () {
+                  setState(() {
+                    dice2_image = Random().nextInt(5) + 1;
+                    _incerment2();
+                    print("Show Max = $s");
+                   
+                  });
+                },
+                child: Image(
+                  image: new AssetImage("../images/d$dice2_image.png"),
+                )),
           ),
-        ],
-      ),
-      SizedBox(
-        height: 20,
-        width:20,
-      ),
-      Row(
-        children: [
-          Expanded(
-            child: FlatButton(onPressed:(){
-              setState(() {
-                dice3_image = Random().nextInt(5)+1;
-              });
-
-
-            },
-                child:
-                Image(image: new AssetImage("../images/d$dice3_image.png"),)
-            ),
-          ),
-          Expanded(
-            child: FlatButton(onPressed:(){
-              setState(() {
-                dice4_image = Random().nextInt(5)+1;
-              });
-
-
-            },
-                child:
-                Image(image: new AssetImage("../images/d$dice4_image.png"),)
-            ),
-          ),
-        ],
-      )
-    ],
-  );
-
+        ],)
+      ],
+    );
   }
-
-
 }
-
-
-

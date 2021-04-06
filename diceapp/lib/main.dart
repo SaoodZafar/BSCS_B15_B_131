e9077af1a99d523b7ac058df5debbeda9b57f31e
unknown <49125967+SaoodZafar@users.noreply.github.com>
2021-04-06 13:10:38 +0500

    adding

--- diceapp/lib/main.dart
+++ diceapp/lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
+import 'package:flutter/widgets.dart';
 void main() {
   return runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
@@ -9,20 +11,106 @@ void main() {
         backgroundColor: Colors.teal,
         title: Text("Dice App"),
       ),
-      body: dice(),
+      body: Dice(),
     ),
   ));
 }
-class dice extends StatefulWidget {
+class Dice extends StatefulWidget {
   @override
-  _diceState createState() => _diceState();
+  _DiceState createState() => _DiceState();
 }
 
-class _diceState extends State<dice>{
+class _DiceState extends State<Dice>{
+  var dice1_image = 1;
+  var dice2_image = 1;
+  var dice3_image = 1;
+  var dice4_image = 1;
+  var counter1 = 0;
+  var counter2 = 0;
+  var n= 0;
+  var x= 0;
+  void _incerment(){
+
+  setState(() {
+    counter1++;
+    counter2++;
+    n = n + dice1_image;
+    x = x + dice2_image;
+  });
+
+
+  }
 @override
   Widget build(BuildContext context) {
   return Column(
-    children: [],
+    children: [
+      Row(
+        children: [
+          Expanded(
+            child: FlatButton(onPressed:(){
+              setState(() {
+                dice1_image = Random().nextInt(5)+1;
+                _incerment();
+                print("Value1 = $counter1");
+                print("Value1 = $n");
+              });
+
+
+            },
+                child:
+            Image(image: new AssetImage("../images/d$dice1_image.png"),)
+            ),
+          ),
+          Expanded(
+            child: FlatButton(onPressed:(){
+              setState(() {
+                dice2_image = Random().nextInt(5)+1;
+                _incerment();
+                print("Value2 = $counter2");
+                print("Value2 = $x");
+              });
+
+
+            },
+                child:
+                Image(image: new AssetImage("../images/d$dice2_image.png"),)
+            ),
+          ),
+        ],
+      ),
+      SizedBox(
+        height: 20,
+        width:20,
+      ),
+      Row(
+        children: [
+          Expanded(
+            child: FlatButton(onPressed:(){
+              setState(() {
+                dice3_image = Random().nextInt(5)+1;
+              });
+
+
+            },
+                child:
+                Image(image: new AssetImage("../images/d$dice3_image.png"),)
+            ),
+          ),
+          Expanded(
+            child: FlatButton(onPressed:(){
+              setState(() {
+                dice4_image = Random().nextInt(5)+1;
+              });
+
+
+            },
+                child:
+                Image(image: new AssetImage("../images/d$dice4_image.png"),)
+            ),
+          ),
+        ],
+      )
+    ],
   );
 
   }

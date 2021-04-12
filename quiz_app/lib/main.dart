63ff1046c720e78bf10ff46f2af3fd11dbd30bc2
unknown <49125967+SaoodZafar@users.noreply.github.com>
2021-04-12 15:05:26 +0500

    class

--- quiz_app/lib/main.dart
+++ quiz_app/lib/main.dart
@@ -11,30 +11,83 @@ void main() {
     ),
   ));
 }
+
 class Quiz extends StatefulWidget {
   @override
   _QuizState createState() => _QuizState();
 }
 
 class _QuizState extends State<Quiz> {
+  List <Icon> scorekeeper=[];
   @override
   Widget build(BuildContext context) {
+
     return Column(
+
+      crossAxisAlignment: CrossAxisAlignment.stretch,
       children: [
-        Expanded(child: Padding(
-          padding: EdgeInsets.all(10.0),
-          child: Text("Question 1",
-            textAlign: TextAlign.center,
-            style: TextStyle(
-              fontSize: 25.0,
-              color: Colors.black
-            ),
+        Expanded(
+            flex: 5,
+            child: Padding(
+              padding: EdgeInsets.all(10.0),
+              child: Center(
+                child: Text(
+                  "Question 1",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 25.0,
+                    color: Colors.black,
+                  ),
+                ),
+              ),
+            )),
+        Expanded(
+            child: Padding(
+                padding: EdgeInsets.all(10.0),
+                child: TextButton(
+                  style: ButtonStyle(backgroundColor: MaterialStateProperty.all(Colors.greenAccent)),
+                  child: Text(
+                    "True",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 20.0,
+                    ),
+                  ),
+                  onPressed: () {
+                    setState(() {
+                      scorekeeper.add(Icon(Icons.check,
+                        color: Colors.green,
+                      ),);
+                    });
 
-          ),
+                  },
+                ))),
+        Expanded(
+            child: Padding(
+                padding: EdgeInsets.all(10.0),
+                child: TextButton(
+                  style:ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all(Colors.redAccent)
+                  ),
+                  child: Text(
+                    "False",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 20.0,
+                    ),
+                  ),
+                  onPressed: () {
+                    setState(() {
+                      scorekeeper.add(Icon(Icons.close,
+                        color: Colors.red,
+                      ));
+
+                    });
 
 
-        ))
-        
+                  },
+                ))),
+        Row(children: scorekeeper,)
       ],
     );
   }

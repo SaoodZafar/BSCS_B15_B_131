b6e9dc2826603f9d2f5c3d89eff3219370ddfa68
unknown <49125967+SaoodZafar@users.noreply.github.com>
2021-05-08 18:16:37 +0500

    ContectUs Page

--- secconed_sessional_tic_tac/lib/Contect_us.dart
+++ secconed_sessional_tic_tac/lib/Contect_us.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:contactus/contactus.dart';
 class contectus extends StatefulWidget {
   @override
   _contectusState createState() => _contectusState();
@@ -7,8 +8,33 @@ class contectus extends StatefulWidget {
 class _contectusState extends State<contectus> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text("Contect us"),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+
+      home: Scaffold(
+      appBar: AppBar(
+        title: Text("Contect Us"),
+        centerTitle: true,
+      ),
+        backgroundColor: Colors.amberAccent,
+        body: ContactUs(
+          logo:AssetImage("images/sa.jpeg") ,
+          email: "saoodzafar5@gail.com",
+          companyName: "BITS",
+          phoneNumber: "03077787942",
+          website: "https://bitsolutions.pk/",
+          githubUserName: "https://github.com/SaoodZafar",
+          tagLine: "Fluter Developer",
+          twitterHandle: "saood7988",
+          instagram: "saoodZafar",
+
+
+
+
+        ),
+      ),
+
+
     );
   }
 }

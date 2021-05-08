63fe160cf1acceca201cc41a080bc5e8adb185d6
unknown <49125967+SaoodZafar@users.noreply.github.com>
2021-05-08 17:07:19 +0500

    Animated Splash Screen  Done

---
+++ secconed_sessional_tic_tac/lib/Contect_us.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+class contectus extends StatefulWidget {
+  @override
+  _contectusState createState() => _contectusState();
+}
+
+class _contectusState extends State<contectus> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text("Contect us"),
+    );
+  }
+}
--- secconed_sessional_tic_tac/lib/main.dart
+++ secconed_sessional_tic_tac/lib/main.dart
@@ -1,18 +1,29 @@
 import 'package:flutter/material.dart';
 import 'package:animated_splash_screen/animated_splash_screen.dart';
- void main ()=>runApp((Splashscreen()));
- class Splashscreen extends StatefulWidget {
-   @override
-   _SplashscreenState createState() => _SplashscreenState();
- }
+import 'package:secconed_sessional_tic_tac/Contect_us.dart';
 
- class _SplashscreenState extends State<Splashscreen> {
-   @override
-   Widget build(BuildContext context) {
-     return MaterialApp(
+void main() => runApp((Splashscreen()));
+
+class Splashscreen extends StatefulWidget {
+  @override
+  _SplashscreenState createState() => _SplashscreenState();
+}
+
+class _SplashscreenState extends State<Splashscreen> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: AnimatedSplashScreen(),
-     );
-   }
- }
+      home: AnimatedSplashScreen.withScreenFunction(
+        splash: Image.asset(
+          ("images/sau.png"),
 
+        ),
+        splashTransition: SplashTransition.rotationTransition,
+        screenFunction: ()async{
+          return contectus();
+        },
+      ),
+    );
+  }
+}

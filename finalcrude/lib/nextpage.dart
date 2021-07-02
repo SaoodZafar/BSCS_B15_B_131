a50371279d253be1160550a0b6882e76526741a8
unknown <49125967+SaoodZafar@users.noreply.github.com>
2021-07-02 13:18:36 +0500

    Add Data

--- finalcrude/lib/nextpage.dart
+++ finalcrude/lib/nextpage.dart
@@ -9,6 +9,110 @@ class nextpages extends StatefulWidget {
 class _nextpagesState extends State<nextpages> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.greenAccent,
+          shadowColor: Colors.amber,
+          title: Text(
+            "Crud App ",
+            style: TextStyle(
+              color: Colors.white,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          centerTitle: true,
+        ),
+        body: Column(
+          children: [
+            Container(
+                margin: EdgeInsets.all(20),
+                child: TextField(
+
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Name',
+                  ),
+                  onChanged: (text) {
+                    setState(() {
+
+                      //you can access nameController in its scope to get
+                      // the value of text entered as shown below
+                      //fullName = nameController.text;
+                    });
+                  },
+                )),
+            Container(
+                margin: EdgeInsets.all(20),
+                child: TextField(
+
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Email',
+                  ),
+                  onChanged: (text) {
+                    setState(() {
+
+                      //you can access nameController in its scope to get
+                      // the value of text entered as shown below
+                      //fullName = nameController.text;
+                    });
+                  },
+                )),
+            Container(
+                margin: EdgeInsets.all(20),
+                child: TextField(
+
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Phone',
+                  ),
+                  onChanged: (text) {
+                    setState(() {
+
+                      //you can access nameController in its scope to get
+                      // the value of text entered as shown below
+                      //fullName = nameController.text;
+                    });
+                  },
+                )),
+            Container(
+                margin: EdgeInsets.all(20),
+                child: TextField(
+
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Address',
+                  ),
+                  onChanged: (text) {
+                    setState(() {
+
+                      //you can access nameController in its scope to get
+                      // the value of text entered as shown below
+                      //fullName = nameController.text;
+                    });
+                  },
+                )),
+            RaisedButton(
+              color: Colors.green,
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10.0)),
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => nextpages()),
+                );
+              },
+              child: Text(
+                "Add To DataBase",
+                style: TextStyle(
+                    color: Colors.white, fontWeight: FontWeight.bold),
+              ),
+            ),
+          ],
+        ),
+
+      ),
+    );
   }
 }

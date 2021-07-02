f2b6e1ff2ef979cfd429e959590e8a82aae1f89c
unknown <49125967+SaoodZafar@users.noreply.github.com>
2021-07-02 09:55:17 +0500

    splash screen

---
+++ finalcrude/lib/mainpage.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+class nextpage extends StatefulWidget {
+  const nextpage({Key? key}) : super(key: key);
+
+  @override
+  _nextpageState createState() => _nextpageState();
+}
+
+class _nextpageState extends State<nextpage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
